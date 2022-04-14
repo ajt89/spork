2650d34dbb356a3a0b3973bea68bcff5ca75083a
ajt89 <ajtuquero@gmail.com>
2022-04-13 22:45:03 -0700

    GH-4: Validate youtube urls

---
+++ tests/test_spotify_models.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+from unittest import TestCase
+
+from spotify.models.track import SpotifyTrack
+from tests.spork_constants import (
+    SPORK_ALBUM_ID,
+    SPORK_ALBUM_LENGTH,
+    SPORK_PLAYLIST_ID,
+    SPORK_PLAYLIST_LENGTH,
+    SPORK_TRACK_ARTISTS,
+    SPORK_TRACK_ID,
+    SPORK_TRACK_LENGTH,
+    SPORK_TRACK_NAME,
+    YOUTUBE_QUERY,
+)
+
+
+class TestYoutubeQuery(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        spotify_track_response = {}
+
+        spotify_fixtures_folder = Path("tests/fixtures/spotify/")
+
+        with open(spotify_fixtures_folder / "track.json") as json_file:
+            spotify_track_response = json.load(json_file)
+        cls.spotify_track = SpotifyTrack(spotify_track_response)
+
+    def test_init(self):
+        self.assertEqual(SPORK_TRACK_ARTISTS, self.spotify_track.artists)
+        self.assertEqual(SPORK_TRACK_NAME, self.spotify_track.name)
+        self.assertEqual(SPORK_TRACK_LENGTH, self.spotify_track.length)
+        self.assertEqual(SPORK_TRACK_ID, self.spotify_track.id)
+
+    def test_get_query_statement(self):
+        query_statement = self.spotify_track.get_query_statement()
+
+        self.assertEqual(YOUTUBE_QUERY, query_statement)
---
+++ tests/test_youtube_models.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+from unittest import TestCase, mock
+
+from tests.spork_constants import YOUTUBE_ID, YOUTUBE_TITLE, YOUTUBE_URL
+from youtube.models.video import YoutubeVideo
+
+
+class TestYoutubeQuery(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        youtube_fixtures_folder = Path("tests/fixtures/youtube/")
+
+        with open(youtube_fixtures_folder / "single_video.json") as json_file:
+            youtube_single_video_response = json.load(json_file)
+
+        item = youtube_single_video_response.get("items")[0]
+        cls.youtube_video = YoutubeVideo(item)
+
+    def test_init(self):
+        self.assertEqual(YOUTUBE_TITLE, self.youtube_video.name)
+        self.assertEqual(YOUTUBE_ID, self.youtube_video.id)
+        self.assertEqual(YOUTUBE_URL, self.youtube_video.url)
+
+    @mock.patch("youtube.models.video.requests.get")
+    def test_validate_url_valid(self, mock_get):
+        mock_get.return_value.text = "This video isn't available anymore"
+
+        self.assertTrue(self.youtube_video.validate_url())
+
+    @mock.patch("youtube.models.video.requests.get")
+    def test_validate_url_invalid(self, mock_get):
+        mock_get.return_value.text = "ok"
+
+        self.assertFalse(self.youtube_video.validate_url())
